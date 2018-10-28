ddb48a6eab4de53c11da41006fc02f44dc86625a
Prashant Kumar <prashant13cse075@gmail.com>
2018-10-28 10:20:37 +0530

    [master] Initial stage

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Navigation from './components/Navigation';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <React.Fragment>
+        <div className="section-1 text-center">
+          <Navigation />
+          <div className="center">
+            <h1>Hody</h1>
+          </div>
+        </div>
+        <div className="section-2">
+          <p>Something i am going to write</p>
+          <button className="btn btn-primary">Close me</button>
+        </div>
+        <div className="section-3">
+          <h1>Hello</h1>
+        </div>
+      </React.Fragment>
     );
   }
 }
