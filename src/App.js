a1f8e530ac46b2372caf039a422952a057825af3
Prashant Kumar <prashant.kumar@sainsburys.co.uk>
2018-11-01 22:30:56 +0530

    [develop] Issue fixed with appjs

--- src/App.js
+++ src/App.js
@@ -19,25 +19,6 @@ class App extends Component {
             mind to it and maintain a positive attitude.” —Lou Holtz
           </h1>
         </div>
-        <div className="section-4">
-          <div className="container-fluid">
-            <div className="row">
-              <div className="col-lg-3 col-xl-3">
-                <div class="section-heading">
-                  <h2>
-                  </h2>
-                </div>
-              </div>
-              <div className="col-lg-9 col-xl-9">
-                <Skill heading="ReactJS" percent="85" />
-                <Skill heading="HTML5" percent="99" />
-                <Skill heading="Python" percent="60" />
-                <Skill heading="MySQL" percent="70" />
-                <Skill heading="MongoDB" percent="70" />
-              </div>
-            </div>
-          </div>
-        </div>
         <Resume />
         <ContactMe />
         <Footer />
